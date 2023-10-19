a8e3909990a4c2886ce1c03519f1385595af9513
JulianMenedez12 <128722932+JulianMenedez12@users.noreply.github.com>
2023-10-19 10:14:18 -0500

    Add files via upload

--- despliegue.php
+++ despliegue.php
@@ -1,3 +1,4 @@
 <?php
 include('funciones.php'); //se incluye el achivo de funciones
-echo comer();//llamado de la funcion comer
+echo contar_usuarios();//llamado de la funcion contar_usuarios
+insertar_personas('9','Enqrique Mendez','19','300632926','87');
--- funciones.php
+++ funciones.php
@@ -1,5 +1,4 @@
 <?php
-echo 'Hola';//impresión 
 function consulta(){
     $conexion=mysqli_connect('127.0.0.1','root','root','facturas');//conexion con el servidor/base de datos
     $salida=0;//inicializacion de variable
@@ -12,17 +11,34 @@ function comer(){
     $menor = 'es menor de edad';
     $rse = '';
     $conexion = mysqli_connect('127.0.0.1', 'root', 'root', 'facturas');
-    $sql = "SELECT 17 AS edad";
+    $sql = "SELECT 19 AS edad";
     $resultado = $conexion->query($sql);
     
     while($fila = mysqli_fetch_assoc($resultado)){
         $salida = $fila['edad'];
         if ($salida >= 18){
-            $salida = $salida . ' ' . $mayor;
+            $salida = $salida.'<br>'.$mayor;
         } else {
-            $salida = $salida . ' ' . $menor;
+            $salida = $salida.'<br>'.$menor;
         }
     }
 
     return $salida;
+}
+function contar_usuarios(){
+    $salida=0;//inicializacion de variable
+    $conexion = mysqli_connect('127.0.0.1', 'root', 'root', 'facturas');//conecta con la base de datos
+    $sql="SELECT count(nombre) FROM usuario";//orden a ejecutar en la base de datos
+    $r=$conexion->query($sql);//almacena los datos obtenidos de la base de datos en una variable
+    //navega en las filas obtenidas
+    while($fila=mysqli_fetch_array($r)){
+        $salida = $fila[0];//almacena datos en la variable
+    }
+    return $salida;//retorna la variable con los datos almacenados
+}
+function insertar_personas($id,$nombre,$edad,$tel,$id_pais){
+    $conexion = mysqli_connect('127.0.0.1', 'root', 'root', 'facturas');//conecta con la base de datos
+    $sql = "INSERT INTO usuario VALUES ('$id','$nombre','$edad','$tel','$id_pais')";
+    $r=$conexion->query($sql);
+    return;
 }
